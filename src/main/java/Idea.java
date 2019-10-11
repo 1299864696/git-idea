bbde644173018a944300a67e673dbce06a02097c
zs <zs@rtyuio.com>
2019-10-11 14:58:02 +0800

    dev分支22222

--- src/main/java/Idea.java
+++ src/main/java/Idea.java
@@ -6,6 +6,11 @@ public class Idea {
     public static void main(String[] args) {
         
         System.out.println("dev");
+        System.out.println("dev");
+        System.out.println("dev");
+        System.out.println("dev");
+
+
     }
 
 
